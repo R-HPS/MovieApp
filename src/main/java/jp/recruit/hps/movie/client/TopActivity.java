b9b27cc81d531ac56ee05749307d56cfa2955974
ippei-takahashi <aimluck.ippei.takahashi@gmail.com>
2013-09-20 16:23:13 +0900

    修正

--- src/main/java/jp/recruit/hps/movie/client/TopActivity.java
+++ src/main/java/jp/recruit/hps/movie/client/TopActivity.java
@@ -7,13 +7,11 @@ import jp.recruit.hps.movie.client.api.RemoteApi;
 import jp.recruit.hps.movie.client.utils.CommonUtils;
 import jp.recruit.hps.movie.client.utils.CompanyAdapter;
 import android.app.Activity;
-import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
@@ -29,39 +27,7 @@ public class TopActivity extends Activity {
 
 		setContentView(R.layout.activity_mypage);
 		new GetCompanyListAsyncTask(this).execute(CommonUtils.TEST_USER_KEY);
-//		findViewById(R.id.search_button).setOnClickListener(
-//				new OnClickListener() {
-//					@Override
-//					public void onClick(View v) {
-//						onSearchRequested();
-//					}
-//				});
 	}
-//	public void setList(List<CompanyV1Dto> list){
-//		ListView lv = (ListView) findViewById(R.id.company_list);
-//		lv.setAdapter(new CompanyAdapter(this, list));]
-//		lv.setOnItemClickListener(new OnItemClickListener() {
-//
-//			@Override
-//			public void onItemClick(AdapterView<?> arg0, View arg1,
-//					int number, long arg3) {
-//				// TODO 自動生成されたメソッド・スタブ
-//				Intent intent = new Intent(TopActivity.this, CompanyInterviewActivity.class);
-//				intent.putExtra(CommonUtils.STRING_EXTRA_USER_KEY, list.get(number).getKey());
-//				intent.putExtra(CommonUtils.STRING_EXTRA_FILE_NAME, list.get(number).getName());
-//				startActivity(intent);
-//					
-//			}
-//		});
-//	}
-//CommonUtils.sampleUserKey
-//	@Override
-//	protected void onNewIntent(Intent intent) {
-//		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-//			String query = intent.getStringExtra(SearchManager.QUERY);
-//			new GetCompanyListAsyncTask(this).execute(query);
-//		}
-//	}
 
 	public class GetCompanyListAsyncTask extends
 			AsyncTask<String, Integer, Boolean> {
@@ -78,8 +44,8 @@ public class TopActivity extends Activity {
 			CompanyEndpoint endpoint = RemoteApi.getCompanyEndpoint();
 			try {
 				CompanyV1DtoCollection collection = endpoint
-						.companyV1EndPoint().searchCompany(query).execute();
-				if (collection != null) {
+						.companyV1EndPoint().getCompanies(query).execute();
+				if (collection != null && collection.getItems() != null) {
 					list = collection.getItems();
 				} else {
 					return false;
@@ -102,11 +68,14 @@ public class TopActivity extends Activity {
 					public void onItemClick(AdapterView<?> arg0, View arg1,
 							int number, long arg3) {
 						// TODO 自動生成されたメソッド・スタブ
-						Intent intent = new Intent(TopActivity.this, CompanyInterviewActivity.class);
-						intent.putExtra(CommonUtils.STRING_EXTRA_USER_KEY, list.get(number).getKey());
-						intent.putExtra(CommonUtils.STRING_EXTRA_FILE_NAME, list.get(number).getName());
+						Intent intent = new Intent(TopActivity.this,
+								CompanyInterviewActivity.class);
+						intent.putExtra(CommonUtils.STRING_EXTRA_USER_KEY, list
+								.get(number).getKey());
+						intent.putExtra(CommonUtils.STRING_EXTRA_FILE_NAME,
+								list.get(number).getName());
 						startActivity(intent);
-							
+
 					}
 				});
 			}
